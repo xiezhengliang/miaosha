7d1f5dcb18e0c908c552f01c6667f5a0b700d030
xiezhengliang <779536513@qq.com>
2018-11-15 18:38:23 +0800

    sql

--- miaosha.sql
+++ miaosha.sql
@@ -16,6 +16,59 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`miaosha` /*!40100 DEFAULT CHARACTER SET
 
 USE `miaosha`;
 
+/*Table structure for table `goods` */
+
+DROP TABLE IF EXISTS `goods`;
+
+CREATE TABLE `goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `goods_name` varchar(16) DEFAULT NULL,
+  `goods_title` varchar(64) DEFAULT NULL,
+  `goods_img` varchar(64) DEFAULT NULL,
+  `goods_detail` longtext,
+  `goods_price` decimal(10,2) DEFAULT NULL,
+  `goods_stock` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `goods` */
+
+insert  into `goods`(`id`,`goods_name`,`goods_title`,`goods_img`,`goods_detail`,`goods_price`,`goods_stock`) values (1,'iphoneX','apple IPhone X','/img/iphonex.png','苹果苹果苹果苹果苹果苹果','8765.00',100),(2,'华为Meta9 ','华为Meta9 4GB+32GB','/img/meta10.png','华为Meta9 4GB+32GB','3212.00',NULL);
+
+/*Table structure for table `miaosha_goods` */
+
+DROP TABLE IF EXISTS `miaosha_goods`;
+
+CREATE TABLE `miaosha_goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `goods_id` bigint(20) DEFAULT NULL,
+  `miaosha_price` decimal(10,2) DEFAULT NULL,
+  `stock_count` int(11) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `miaosha_goods` */
+
+insert  into `miaosha_goods`(`id`,`goods_id`,`miaosha_price`,`stock_count`,`start_date`,`end_date`) values (1,1,'0.01',2,'2018-11-15 16:04:02','2018-11-15 20:52:13'),(2,2,'0.01',9,'2018-11-14 14:52:07','2018-11-15 14:55:21');
+
+/*Table structure for table `miaosha_order` */
+
+DROP TABLE IF EXISTS `miaosha_order`;
+
+CREATE TABLE `miaosha_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `order_id` bigint(20) DEFAULT NULL,
+  `goods_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `miaosha_order` */
+
+insert  into `miaosha_order`(`id`,`user_id`,`order_id`,`goods_id`) values (7,18819470271,1,1);
+
 /*Table structure for table `miaosha_user` */
 
 DROP TABLE IF EXISTS `miaosha_user`;
@@ -35,6 +88,29 @@ CREATE TABLE `miaosha_user` (
 /*Data for the table `miaosha_user` */
 
 insert  into `miaosha_user`(`id`,`nickname`,`password`,`salt`,`head`,`register_date`,`last_login_date`,`login_count`) values (18819470271,'xiezhengliang','b7797cce01b4b131b433b6acf4add449','1a2b3c4d',NULL,'2018-11-14 11:32:21','2018-11-14 11:32:24',1);
+
+/*Table structure for table `order_info` */
+
+DROP TABLE IF EXISTS `order_info`;
+
+CREATE TABLE `order_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `goods_id` bigint(20) DEFAULT NULL,
+  `delivery_addr_id` bigint(20) DEFAULT NULL,
+  `goods_name` varchar(16) DEFAULT NULL,
+  `goods_count` int(11) DEFAULT NULL,
+  `goods_price` decimal(10,2) DEFAULT NULL,
+  `order_channel` tinyint(4) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `pay_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*Data for the table `order_info` */
+
+insert  into `order_info`(`id`,`user_id`,`goods_id`,`delivery_addr_id`,`goods_name`,`goods_count`,`goods_price`,`order_channel`,`status`,`create_date`,`pay_date`) values (9,18819470271,1,NULL,'iphoneX',1,'0.01',1,0,'2018-11-15 18:29:43',NULL);
 
 /*Table structure for table `user` */
 
